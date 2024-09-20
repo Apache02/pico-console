2e2e1f2221519cb7431096a90e9e40696ab262b2
Aleksey Priymachuk <gxad02@gmail.com>
2024-09-21 01:18:26 +0300

    feat: added flash info

--- src/console/commands/examples.cpp
+++ src/console/commands/examples.cpp
@@ -3,8 +3,15 @@
 #include <stdio.h>
 #include "hardware/adc.h"
 #include "hardware/structs/sysinfo.h"
+#include "hardware/flash.h"
 #include "pico/unique_id.h"
 
+
+static struct {
+    uint8_t manufacturer;
+    uint8_t memory_type;
+    uint8_t capacity;
+} flash_JEDEC_ID;
 
 void command_echo(Console &c) {
     printf("%s\n", c.packet.take_rest_string());
@@ -24,6 +31,19 @@ void command_sensors(Console &c) {
     printf("Onboard temperature: %+.01f °C\n", tempC);
 }
 
+static void __attribute__((constructor)) _read_flash_JEDEC_ID_on_boot() {
+    uint8_t txbuf[4] = {0};
+    uint8_t rxbuf[4] = {0};
+    // Winbond W25Q series pdf
+    // 8.2.29 Read JEDEC ID (9Fh)
+    txbuf[0] = 0x9f;
+    flash_do_cmd(txbuf, rxbuf, 4);
+
+    flash_JEDEC_ID.manufacturer = rxbuf[1];
+    flash_JEDEC_ID.memory_type = rxbuf[2];
+    flash_JEDEC_ID.capacity = rxbuf[3];
+}
+
 void command_chip_id(Console &c) {
     uint32_t chip_id = sysinfo_hw->chip_id;
     uint16_t manufacturer = (chip_id >> 0) & 0x0fff;
@@ -37,4 +57,8 @@ void command_chip_id(Console &c) {
     char board_id[32];
     pico_get_unique_board_id_string(board_id, sizeof(board_id));
     printf("Unique board identifier: %s\n", board_id);
+
+    printf("Flash Manufacturer: 0x%02X\n", flash_JEDEC_ID.manufacturer);
+    printf("Flash Memory Type: 0x%02X\n", flash_JEDEC_ID.memory_type);
+    printf("Flash Capacity: 0x%02X (%d MB)\n", flash_JEDEC_ID.capacity, (1 << flash_JEDEC_ID.capacity) / (1 << 20));
 }
